2bebc7d7134988a4e3c23be60b4feddd0489e00a
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-09-21 15:51:40 -0500

    Add service to make calls to the Notion API

---
+++ src/main/java/com/portfolio/services/NotionService.java
@@ -0,0 +1,40 @@
+package com.portfolio.services;
+
+import com.portfolio.config.NotionConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class NotionService {
+
+    private final WebClient webClient;
+    private NotionConfigProperties notionConfigProperties;
+
+    @Autowired
+    public NotionService(WebClient.Builder webClientBuilder, NotionConfigProperties notionConfig) {
+        this.notionConfigProperties = notionConfig;
+        this.webClient = webClientBuilder.build();
+    }
+
+    public Mono<String> getDatabaseById() {
+        return webClient
+                .get()
+                .uri("v1/databases/" + notionConfigProperties.getDatabaseId())
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    public String getPagesFromDatabase() {
+        return webClient
+                .post()
+                .uri("v1/databases/" + notionConfigProperties.getDatabaseId() + "/query")
+                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue("")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+}
