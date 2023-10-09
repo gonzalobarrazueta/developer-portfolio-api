0a84fd2615d308232fcc4864e7684295a5463afc
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-10-09 12:01:42 -0500

    Implement CRUD operations

--- src/main/java/com/portfolio/controllers/ProjectController.java
+++ src/main/java/com/portfolio/controllers/ProjectController.java
@@ -1,12 +1,15 @@
 package com.portfolio.controllers;
 
 import com.portfolio.models.dtos.ProjectDTO;
+import com.portfolio.persitance.entities.Project;
 import com.portfolio.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/projects")
@@ -25,9 +28,41 @@ public class ProjectController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<ProjectDTO> getProjectById(@PathVariable Integer id) {
+
+        Optional<ProjectDTO> response = projectService.getProjectById(id);
+
+        if (response.isPresent()) {
+            return ResponseEntity.ok(response.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @PostMapping(value = "", produces = "application/json")
     public ResponseEntity<ProjectDTO> save(@RequestBody ProjectDTO projectDTO) {
-        ProjectDTO response = projectService.save(projectDTO);
-        return ResponseEntity.ok(response);
+        projectService.save(projectDTO);
+        return ResponseEntity.created(URI.create("api/v1/projects")).build();
+    }
+
+    @PutMapping(value = "", produces = "application/json")
+    public ResponseEntity update(@RequestBody Project project) {
+       try {
+           projectService.update(project);
+       } catch (IllegalArgumentException e) {
+           return ResponseEntity.badRequest().body(e.getMessage());
+       }
+       return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity delete(@PathVariable Integer id) {
+        try {
+            projectService.delete(id);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/portfolio/services/ProjectService.java
+++ src/main/java/com/portfolio/services/ProjectService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -29,8 +30,29 @@ public class ProjectService {
                 .toList();
     }
 
+    public Optional<ProjectDTO> getProjectById(Integer id) {
+        return projectDao.getById(id)
+                .map(project -> projectMapper.projectToProjectDTO(project));
+    }
+
     public ProjectDTO save(ProjectDTO projectDTO) {
         Project project = projectMapper.projectDTOToProject(projectDTO);
         return projectMapper.projectToProjectDTO(projectDao.save(project));
+    }
+
+    public boolean update(Project project) {
+        if (projectDao.update(project)) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Project with id " + project.getId() + " does not exist");
+        }
+    }
+
+    public boolean delete(Integer id) {
+        if (projectDao.deleteById(id)) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Project with id " + id + " does not exist");
+        }
     }
 }
