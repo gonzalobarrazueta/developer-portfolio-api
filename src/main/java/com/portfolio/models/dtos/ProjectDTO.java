eef16a5eb390ac9d90199058b2df84f420803d87
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-12-05 21:38:34 -0500

    Add 'technologies' attribute to ProjectDTO and return technologies in mapper method

--- src/main/java/com/portfolio/models/dtos/ProjectDTO.java
+++ src/main/java/com/portfolio/models/dtos/ProjectDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,4 +16,5 @@ public class ProjectDTO {
     private String title;
     private String description;
     private String imageUrl;
+    private List<TechnologyDTO> technologies;
 }
--- src/main/java/com/portfolio/models/dtos/ProjectMapper.java
+++ src/main/java/com/portfolio/models/dtos/ProjectMapper.java
@@ -3,15 +3,28 @@ package com.portfolio.models.dtos;
 import com.portfolio.persitance.entities.Project;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectMapper {
 
     public static ProjectDTO projectToProjectDTO(Project project) {
+
+        List<TechnologyDTO> technologies = List.of();
+
+        if (project.getTechnologies() != null) {
+            technologies = project.getTechnologies()
+                    .stream()
+                    .map(technology -> TechnologyMapper.technologyToTechnologyDTO(technology))
+                    .toList();
+        }
+
         return new ProjectDTO(
                 project.getPageId(),
                 project.getTitle(),
                 project.getDescription(),
-                project.getImageUrl()
+                project.getImageUrl(),
+                technologies
         );
     }
 
